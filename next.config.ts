070180db1cc10df2413a45cbe442d3990d41bcf1
Tina Huynh <tinamchuynh@gmail.com>
2025-05-25 14:58:24 -0700

    Add initial configuration files for Next.js project
    - Create components.json for component configuration
    - Update eslint.config.mjs to include additional rules
    - Modify next.config.ts to define remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+const imageHosts = [
+  "images.unsplash.com",
+  "tailwindcss.com",
+  "plus.unsplash.com",
+  "img.freepik.com",
+  "rainn.org",
+  "media.istockphoto.com",
+  "cdn.pixabay.com",
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: imageHosts.map((hostname) => ({
+      protocol: "https",
+      hostname,
+      port: "",
+      pathname: "/**",
+    })),
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
