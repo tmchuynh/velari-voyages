32d2d4f9dde21bc9a7b3c8413049015578419c85
Tina Huynh <tinamchuynh@gmail.com>
2025-06-02 00:24:48 -0700

    Adds Loading component for improved user experience during data fetching

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="border-primary border-t-2 border-b-2 rounded-full w-16 h-16 animate-spin"></div>
+    </div>
+  );
+}
