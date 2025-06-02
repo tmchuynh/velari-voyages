be59259ace99b9b22e46a23f8650c5fc2d54a32f
Tina Huynh <tinamchuynh@gmail.com>
2025-06-02 00:35:32 -0700

    Refactors TeamMember interface and renames TourGuide to ContactPersonnel, updating optional properties

--- src/lib/interfaces/people/staff.ts
+++ src/lib/interfaces/people/staff.ts
@@ -2,13 +2,13 @@ import { ContactInfo } from "@/lib/types/types";
 
 export interface TeamMember {
   name: string;
-  bio: string;
-  description?: string;
+  bio?: string;
+  description: string;
   languages: string[];
-  experienceYears: number;
+  experienceYears?: number;
   profileImage: string;
 }
 
-export interface TourGuide extends TeamMember {
+export interface ContactPersonnel extends TeamMember {
   contact: ContactInfo;
 }
