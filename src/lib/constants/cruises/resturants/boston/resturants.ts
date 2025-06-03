581dcef9584ba4f74c6c36e04ebff6fa75c7a15f
Tina Huynh <tinamchuynh@gmail.com>
2025-06-03 13:08:24 -0700

    Remove restaurant and menu data for Galveston and Hong Kong locations

--- src/lib/constants/cruises/resturants/boston/resturants.ts
+++
@@ -1,3 +0,0 @@
-import { Resturant } from "@/lib/types/types";
-
-export const bostonResturants: Resturant[] = [];
--- src/lib/constants/cruises/resturants/dublin/resturants.ts
+++
@@ -1,3 +0,0 @@
-import { Resturant } from "@/lib/types/types";
-
-export const dublinResturants: Resturant[] = [];
