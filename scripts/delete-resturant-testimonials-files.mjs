afa0e7e115254bd680c083bc131aee1fbb43f2fc
Tina Huynh <tinamchuynh@gmail.com>
2025-06-03 15:37:18 -0700

    feat: add scripts for creating and deleting restaurant testimonials files

--- scripts/delete-resturant-testimonials-files.mjs
+++ scripts/delete-resturant-testimonials-files.mjs
@@ -1,5 +1,10 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the equivalent of __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Base directory where restaurant files are located
 const baseDir = path.join(
