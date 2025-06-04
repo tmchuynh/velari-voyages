12d4a0d293ab5dfd0c095a8341c2ca92035ee9d0
Tina Huynh <tinamchuynh@gmail.com>
2025-06-03 19:43:44 -0700

    Adds restaurant menu rendering with enhanced formatting
    Implements a detailed rendering of restaurant menus, including
    dynamic categorization and item attributes such as dietary
    labels (Vegetarian, Vegan, Gluten Free, Halal, Kosher).
    
    Introduces a new currency formatting utility for item prices
    to improve usability and presentation.
    
    Enhances the overall layout for better visual organization
    of menu sections.

--- src/app/cruises/restaurants/[resturant]/page.tsx
+++ src/app/cruises/restaurants/[resturant]/page.tsx
@@ -2,8 +2,11 @@
 
 import Loading from "@/components/Loading";
 import { RestaurantMenu } from "@/lib/types/types";
-import { formatKebebToTitleCase } from "@/lib/utils/format";
-import { getRestaurantMenuByName } from "@/lib/utils/get";
+import {
+  formatKebebToTitleCase,
+  formatNumberToCurrency,
+} from "@/lib/utils/format.ts";
+import { getRestaurantMenuByName } from "@/lib/utils/get.ts";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -69,6 +72,139 @@ export default function RestaurantMenuPage() {
               {formatKebebToTitleCase(restaurant).replace("menu", "")} Menu
             </h1>
           </header>
+
+          <section>
+            {restaurantData.map((menu, index) => {
+              let split = 4; // Default split value
+
+              if (menu.category.length > 5) {
+                split = 5;
+              } else if (menu.category.length === 2) {
+                split = 1;
+              } else {
+                split = 2;
+              }
+
+              return (
+                <div key={index} className="mb-8">
+                  <h2>{menu.title}</h2>
+                  <p>{menu.description}</p>
+                  <div className="gap-4 grid lg:grid-cols-2">
+                    <section>
+                      {menu.category
+                        .slice(0, split)
+                        .map((menuCategory, itemIndex) => (
+                          <section key={itemIndex} className="mt-4">
+                            <h3>{menuCategory.name}</h3>
+                            {menuCategory.items.map((item, itemIdx) => (
+                              <div key={itemIdx} className="py-1 w-full">
+                                <div className="flex-col space-y-1">
+                                  <div className="flex justify-between items-center">
+                                    <div className="flex flex-wrap items-center">
+                                      <h4 className="no-underline">
+                                        {item.name}
+                                      </h4>
+                                      {item.isVegetarian && (
+                                        <span className="ml-2 text-green-600">
+                                          (Vegetarian)
+                                        </span>
+                                      )}
+                                      {item.isVegan && (
+                                        <span className="ml-2 text-green-600">
+                                          (Vegan)
+                                        </span>
+                                      )}
+                                      {item.isGlutenFree && (
+                                        <span className="ml-2 text-blue-600">
+                                          (Gluten Free)
+                                        </span>
+                                      )}
+                                      {item.isHalal && (
+                                        <span className="ml-2 text-purple-600">
+                                          (Halal)
+                                        </span>
+                                      )}
+                                      {item.isKosher && (
+                                        <span className="ml-2 text-orange-600">
+                                          (Kosher)
+                                        </span>
+                                      )}
+                                    </div>
+                                    <p>
+                                      {formatNumberToCurrency(item.price, 2, 2)}
+                                    </p>
+                                  </div>
+                                  <div>
+                                    {item.description && (
+                                      <p>{" - " + item.description}</p>
+                                    )}
+                                  </div>
+                                </div>
+                              </div>
+                            ))}
+                          </section>
+                        ))}
+                    </section>
+                    <section>
+                      {menu.category
+                        .slice(split, menu.category.length)
+                        .map((menuCategory, itemIndex) => (
+                          <section key={itemIndex} className="mt-4">
+                            <h3>{menuCategory.name}</h3>
+                            {menuCategory.items.map((item, itemIdx) => (
+                              <div key={itemIdx} className="py-1">
+                                <div className="flex-col space-y-1">
+                                  <div className="flex justify-between items-center">
+                                    <div className="flex flex-wrap items-center">
+                                      <h4 className="no-underline">
+                                        {item.name}
+                                      </h4>
+                                      {item.isVegetarian && (
+                                        <span className="ml-2 text-green-600">
+                                          (Vegetarian)
+                                        </span>
+                                      )}
+                                      {item.isVegan && (
+                                        <span className="ml-2 text-green-600">
+                                          (Vegan)
+                                        </span>
+                                      )}
+                                      {item.isGlutenFree && (
+                                        <span className="ml-2 text-blue-600">
+                                          (Gluten Free)
+                                        </span>
+                                      )}
+                                      {item.isHalal && (
+                                        <span className="ml-2 text-purple-600">
+                                          (Halal)
+                                        </span>
+                                      )}
+                                      {item.isKosher && (
+                                        <span className="ml-2 text-orange-600">
+                                          (Kosher)
+                                        </span>
+                                      )}
+                                    </div>
+                                    <p>
+                                      {formatNumberToCurrency(item.price, 2, 2)}
+                                    </p>
+                                  </div>
+                                  <div>
+                                    {item.description && (
+                                      <p>{" - " + item.description}</p>
+                                    )}
+                                  </div>
+                                </div>
+                              </div>
+                            ))}
+                          </section>
+                        ))}
+                    </section>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
         </div>
       )}
     </>
