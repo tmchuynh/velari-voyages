e6b9f0751bcad7e3691c39591952c05152908a16
Tina Huynh <tinamchuynh@gmail.com>
2025-06-04 03:34:13 -0700

    feat: adjust grid layout for restaurant cards to improve responsiveness

--- src/app/cruises/restaurants/[city]/page.tsx
+++ src/app/cruises/restaurants/[city]/page.tsx
@@ -52,7 +52,7 @@ export default function CityRestaurantsPage() {
 
           <section>
             {allRestaurants.length > 0 && (
-              <div className="gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              <div className="gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                 {allRestaurants.map((restaurant, index) => (
                   <RestaurantDetailsCard
                     restaurant={restaurant}
