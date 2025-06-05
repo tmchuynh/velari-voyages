87b4b75d984c9a390031e4aa1dd99e29b665bb14
Tina Huynh <tinamchuynh@gmail.com>
2025-06-05 15:19:54 -0700

    Adds Cruise Vessels page component with initial content structure

---
+++ src/app/cruises/information/cruise-vessels/page.tsx
@@ -0,0 +1,9 @@
+export default function CruiseVesselsPage() {
+  return (
+    <div>
+      <h1>Cruise Vessels</h1>
+      <p>Information about the cruise vessels will be displayed here.</p>
+      {/* Additional content can be added here */}
+    </div>
+  );
+}
