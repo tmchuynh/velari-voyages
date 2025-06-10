7509575fc6ce2b5611bea021ba03242bd1b8f5ae
Tina Huynh <tinamchuynh@gmail.com>
2025-06-09 18:10:54 -0700

    Adds City interface to manage city-related data
    Introduces a new interface for representing city attributes, including optional fields for region, state, quote, subtitle, and popularity.
    
    This enhancement aims to support better organization and management of city-related information within the application.

--- src/lib/interfaces/services/attractions.ts
+++ src/lib/interfaces/services/attractions.ts
@@ -1,5 +1,15 @@
 import { Details, Flags } from "@/lib/types/types";
 
+export interface City {
+  city: string;
+  region?: string;
+  state?: string;
+  country: string;
+  quote?: string; // Optional quote for the city
+  subtitle?: string; // Optional subtitle for the city
+  isPopular?: boolean; // Optional flag to indicate if the city is popular
+}
+
 export interface CityAttractions {
   subtitle?: string;
   isPopular?: boolean;
